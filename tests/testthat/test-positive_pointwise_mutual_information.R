d4a831d4acab0eaf608ecc544620144e42cb4782
Chris Cox <chriscox@lsu.edu>
2024-08-03 10:41:29 -0500

    Wrote tests for ppmi

--- tests/testthat/test-positive_pointwise_mutual_information.R
+++ tests/testthat/test-positive_pointwise_mutual_information.R
@@ -7,3 +7,19 @@ test_that("ppmi is zero when N = 1", {
   expected <- setNames(rep(0, 10), letters[1:10])
   expect_equal(observed, expected)
 })
+
+test_that("ppmi is one when N = 2 and responses are unique (p == rsum)", {
+  labels <- letters[1:10]
+  p <- setNames(seq(0.1, 1.0, by = 0.1), labels)
+  observed <- ppmi(p = p, rsum = p, N = 2)
+  expected <- setNames(rep(1, 10), labels)
+  expect_equal(observed, expected)
+})
+
+test_that("ppmi is one when N = 4 and responses are shared by two cues (p == rsum*2)", {
+  labels <- letters[1:10]
+  p <- setNames(seq(0.1, 1.0, by = 0.1), labels)
+  observed <- ppmi(p = p, rsum = p * 2, N = 4)
+  expected <- setNames(rep(1, 10), labels)
+  expect_equal(observed, expected)
+})
